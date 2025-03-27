26c40ac17492dc6ca383cd06a0c4ec5ed18421a3
WilliamCommandeur <william.commandeur@gmail.com>
2025-03-27 15:40:08 +0100

    add types, send_request method

--- app/services/lighting.py
+++ app/services/lighting.py
@@ -1,7 +1,9 @@
 import requests
 import os
 from dotenv import load_dotenv
+from app.models.types import RequestData, Capability, CapabilityType
 import uuid
+import json
 
 load_dotenv()
 
@@ -26,55 +28,16 @@ class LightingService:
                 print("No devices found")
                 return None
             
-            print(f"Devices found : {devices}")
+            print("Devices found")
             return devices[0]
         
         else:
             print(f"Error : {response.status_code} : {response.text}")
             return None
-    
-    def turn_on(self, device_id: str, model: str):
-        data = {
-            "requestId": "uuid",
-            "payload": {
-                "device": device_id,
-                "sku": model,
-                "capability": {
-                    "type": "devices.capabilities.on_off",
-                    "instance": "powerSwitch",
-                    "value":1
-                }
-            }
-        }
+        
+    def send_request(self, request_data: RequestData):
+        """Send POST request with structured data"""
+        response = requests.post(self.api_url_control, json=request_data, headers=self.headers)
+        print(f"Response : {response.status_code}")
 
-        response = requests.post(self.api_url_control, json=data, headers=self.headers)
-        print(f"Response : {response}")
-    
-    def turn_off(self, device_id : str, model: str):
-        data = {
-            "requestId": str(uuid.uuid4()),
-            "payload": {
-                "device": device_id,
-                "sku": model,
-                "capability": {
-                    "type": "devices.capabilities.on_off",
-                    "instance": "powerSwitch",
-                    "value":0
-                }
-            }
-        }
-
-        response = requests.post(self.api_url_control, json=data, headers=self.headers)
-
-
-if __name__ == "__main__":
-    lighting_service = LightingService()
-    device = lighting_service.get_device()
-    print(f"Device : {device}")
-    if device:
-        device_id = device['device']
-        model = device['sku']
-        lighting_service.turn_off(device_id=device_id, model=model)
-    else:
-        print(f"Test échoué")
     
